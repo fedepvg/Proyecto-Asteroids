212940b1f03337cb20dbea7859350b7962fc11c6
fedepvg <federicopvg@gmail.com>
2018-09-28 12:21:59 -0300

    cambios leves

--- Asteroids/src/objetos/nave.cpp
+++ Asteroids/src/objetos/nave.cpp
@@ -10,31 +10,24 @@ namespace Juego {
 		float velocidadJug = 0;
 		float rotacion = 0;
 
-		Jug crearJugador(int numJug) {
-			if (numJug != 1) {
-				nave.pos = { 0 + tamanioXJug, ((float)GetScreenHeight() - tamanioYJug) / 2 };
-			}
-			else {
-				nave.pos = { (float)GetScreenWidth() - 2 * tamanioXJug, ((float)GetScreenHeight() - tamanioYJug) / 2 };
-			}
-
-			nave.tamanio = { tamanioXJug, tamanioYJug };
+		Jug crearJugador(/*int numJug*/) {
+			
+			nave.pos = { GetScreenWidth()/2, GetScreenHeight()/2 };
 
 			return nave;
 		}
 
 		void moverNave() {
 			if (IsKeyDown(KEY_UP)) nave.pos.y -= velocidadJug * GetFrameTime();
-			if ((nave.pos.y) <= 0) nave.pos.y = 0;
 			if (IsKeyDown(KEY_DOWN)) nave.pos.y += velocidadJug * GetFrameTime();
-			if ((nave.pos.y + nave.tamanio.y) >= GetScreenHeight()) nave.pos.y = GetScreenHeight() - nave.tamanio.y;
 
 			if (IsKeyDown(KEY_LEFT)) rotacion -= 150 *GetFrameTime();
 			if (IsKeyDown(KEY_RIGHT)) rotacion += 150*GetFrameTime();
 		}
 
 		void dibujarNave() {
-			DrawPoly({ 50,50}, 3, 20, rotacion, WHITE);
+
+			//DrawPoly({ 50,50}, 3, 20, rotacion, WHITE);
 			//DrawRectangleV(nave.pos, nave.tamanio, WHITE);
 		}
 
@@ -43,9 +36,8 @@ namespace Juego {
 			tamanioYJug = (float)(GetScreenHeight()*15.0 / 100.0);
 			velocidadJug = (float)(GetScreenHeight() * 80.0 / 100.0);
 
-			for (int i = 0; i < cantJug; i++) {
-				crearJugador(i);
-			}
+			crearJugador();
+			
 			PantallaJuego::fase = PantallaJuego::inicio;
 		}
 	}
--- Asteroids/src/objetos/nave.h
+++ Asteroids/src/objetos/nave.h
@@ -5,16 +5,25 @@
 
 namespace Juego {
 	namespace Nave {
-		struct Jug {
+		/*struct Jug {
 			Vector2 pos;
 			Vector2 tamanio;
+		};*/
+
+		struct Jug {
+			Vector2 pos;
+			Vector2 velocidad;
+			float aceleracion;
+			float rotacion;
+			//Vector3 collider;
+			Color color;
 		};
 
 		const int cantJug = 2;
 		extern float tamanioXJug;
 		extern float tamanioYJug;
 		extern Jug nave;
-		extern Jug crearJugador(int numJug);
+		extern Jug crearJugador/*(int numJug)*/;
 		extern void moverNave();
 		extern void dibujarNave();
 		extern void inicializarJug();
